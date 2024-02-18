665cb87be5906054615e2796dc53c5734f9c339e
besufikad <07yilmabese@gmail.com>
2024-02-18 21:19:55 +0300

    t 5 & 6 and edited  t 4

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,17 +6,20 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns a salted, hashed password, which is a byte string """
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-
-    return hashed
+    """
+    The function `hash_password` takes a string password as input,
+    encodes it, and then generates a hashed password using bcrypt
+    with a randomly generated salt.
+    """
+    encode = password.encode('utf-8')
+    return bcrypt.hashpw(encode, bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Validates the provided password matches the hashed password """
-    valid = False
-    encoded = password.encode()
-    if bcrypt.checkpw(encoded, hashed_password):
-        valid = True
-    return valid
+    """
+    The function `is_valid` checks if a given password matches a
+    hashed password using the bcrypt algorithm.
+    """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
