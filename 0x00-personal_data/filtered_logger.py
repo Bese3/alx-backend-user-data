41ab74e796296a363eec5e1de8f5e0eacb080b28
besufikad <07yilmabese@gmail.com>
2024-02-18 16:40:32 +0300

    t 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Module for handling Personal Data
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -20,3 +21,25 @@ def filter_datum(fields: List[str],
         my_str = re.sub(pattern.group(0), repl, my_str)
     # my_str = re.sub(';', separator, my_str)
     return my_str
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        The `format` function in the given Python code snippet
+        redacts sensitive information from a log record before
+        formatting it.
+        """
+        log = super(RedactingFormatter, self).format(record=record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
