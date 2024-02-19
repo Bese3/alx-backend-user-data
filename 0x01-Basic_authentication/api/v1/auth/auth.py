e1158fa774a33f6a4c073d9974975303f7febfc8
besufikad <07yilmabese@gmail.com>
2024-02-19 11:27:48 +0300

    t 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,6 +13,16 @@ class Auth:
         """
         checks if url needs authenthication
         """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path == '/api/v1/status/' and path in excluded_paths:
+            return False
+        if path not in excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
         return False
 
     def authorization_header(self, request=None) -> str:
