cbb45e0f429a827b2b41a52b6c7ae1f5d737edb6
besufikad <07yilmabese@gmail.com>
2024-02-22 13:40:45 +0300

    t 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Session Authentication module
 '''
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -32,6 +33,20 @@ class SessionAuth(Auth):
         """
         if session_id is None:
             return
-        if not isinstance(session_id, str):
+        if not isinstance(str(session_id), str):
             return
         return SessionAuth.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """
+        This Python function retrieves the current user based on
+        the session ID provided in the request.
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return
+        user = User.get(user_id)
+        return user
