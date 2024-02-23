da12b6cfdee220b9c89aa29aa30aed97759aa413
besufikad <07yilmabese@gmail.com>
2024-02-23 21:09:36 +0300

    t 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         This function adds a new user to the database with the
         provided email and hashed password.
