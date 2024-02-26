969820d604185108750d3e58957efeffd37b1228
besufikad <07yilmabese@gmail.com>
2024-02-26 18:44:33 +0300

    t 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,6 +70,6 @@ class Auth:
             user = self._db.find_user_by(**{'email': email})
         except (InvalidRequestError, NoResultFound):
             return
-        s_id = _generate_uuid()
-        self._db.update_user(user.id, **{'session_id': s_id})
-        return s_id
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, **{'session_id': session_id})
+        return session_id
