1fddbff694baae0e0a3a60bbc40aa4aed83c787b
besufikad <07yilmabese@gmail.com>
2024-02-26 12:32:50 +0300

    t 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 """API Routes for Authentication Service"""
-# from auth import Auth
-from flask import Flask, jsonify, make_response
+from auth import Auth
+from flask import (
+                    Flask,
+                    jsonify,
+                    make_response,
+                    request
+                  )
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -14,6 +20,22 @@ def hello():
     "Bienvenue" and a status code of 200.
     """
     return make_response(jsonify({'message': 'Bienvenue'}), 200)
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    '''
+    creating user using Auth module
+    '''
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return make_response(jsonify(
+            {"message": "email already registered"}), 400)
+    return make_response(jsonify(
+        {"email": F"{email}", "message": "user created"}), 200)
 
 
 if __name__ == "__main__":
